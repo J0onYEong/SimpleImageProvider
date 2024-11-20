20f341b380b77971083f40f6d39f89a8634db17b
J0onYEong <cjy2134@naver.com>
2024-11-20 14:51:40 +0900

    SimpleImageProvider 프로토콜구현

--- Sources/SimpleImageProvider/SimpleImageProvider.swift
+++ Sources/SimpleImageProvider/SimpleImageProvider.swift
@@ -1,2 +1,9 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import UIKit
+
+protocol SimpleImageProvider {
+    
+    func requestImage(url: String, size: CGSize) async -> UIImage
+}
